79a24c57228ca71752a74ebfb02314533a4944e5
Mauro Doneda <mauro.doneda@gmail.com>
2019-11-28 11:18:50 +0000

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SalesWebMvc.Models
 {
@@ -9,7 +9,29 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inital, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inital, final));
+        }
 
     }
 }
